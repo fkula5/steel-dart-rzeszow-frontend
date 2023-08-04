7a4f968482f6582d90c78bdc6533fba952d8ebbe
Tucznik2 <f3kula@gmail.com>
2023-08-04 03:45:42 +0200

    feat: initial commit, statis home page

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+    <header>
+        <div class="wrapper">
+            <svg class="logo" viewBox="0 0 11.224 22.651" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <linearGradient id="a">
+                        <stop stop-color="#64748b" offset="0" />
+                        <stop stop-color="#fff" offset=".5" />
+                        <stop stop-color="#64748b" offset="1" />
+                    </linearGradient>
+                    <linearGradient id="b">
+                        <stop stop-color="#fff" offset="0" />
+                        <stop stop-color="#94a3b8" offset="1" />
+                    </linearGradient>
+                    <linearGradient id="c">
+                        <stop stop-color="#991b1b" offset="0" />
+                        <stop stop-color="#dc2626" offset="1" />
+                    </linearGradient>
+                </defs>
+                <path d="M1.471 4.4h.8v4.4h-.8z" fill="url(#a)" />
+                <path d="M5.212 4.4h.8v4.4h-.8z" fill="url(#a)" />
+                <path d="M8.953 4.4h.8v4.4h-.8z" fill="url(#a)" />
+                <path d="M1.871 0 .201 1.213v2.064l1.67 2.299z" fill="url(#b)" />
+                <path d="m1.871 0 1.67 1.213v2.064l-1.67 2.299z" fill="url(#c)" />
+                <path d="m5.612 0-1.67 1.213v2.064l1.67 2.299z" fill="url(#b)" />
+                <path d="m5.612 0 1.67 1.213v2.064l-1.67 2.299z" fill="url(#c)" />
+                <path d="m9.353 0-1.67 1.213v2.064l1.67 2.299z" fill="url(#b)" />
+                <path d="m9.353 0 1.67 1.213v2.064l-1.67 2.299z" fill="url(#c)" />
+                <path
+                    d="M5.572 22.651c2.427-1.79 5.65.29 5.65-3.068L11.224 8.8H.007L0 19.583c0 3.358 3.422 1.237 5.572 3.068z"
+                    fill="#13539e"
+                />
+                <path
+                    d="m4.333 19.285 3.468.012-1.721-4.014 4.01 1.627.005-4.19-4.003 1.622 1.756-3.87-4.258-.002 1.677 3.873-3.984-1.65v4.317l3.984-1.74-1.727 4.014z"
+                    fill="#fff"
+                />
+            </svg>
+            <nav>
+                <RouterLink to="/">Strona główna</RouterLink>
+                <RouterLink to="/">Dodaj spotkanie</RouterLink>
+                <RouterLink to="/">Zaloguj</RouterLink>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    background-color: #2e2e2e;
+}
+.wrapper {
+    width: 100%;
+    height: 100px;
+    max-height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+}
+.logo {
+    max-height: 100%;
+}
+nav {
+    display: flex;
+    gap: 10px;
+}
+nav > a {
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: bold;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+const games = [
+    {
+        date: '2023-07-01',
+        playerOne: { name: 'John', surname: 'Doe' },
+        score: '6:4',
+        playerTwo: { name: 'Jane', surname: 'Smith' }
+    },
+    {
+        date: '2023-07-02',
+        playerOne: { name: 'Alice', surname: 'Johnson' },
+        score: '6:3',
+        playerTwo: { name: 'Bob', surname: 'Williams' }
+    },
+    {
+        date: '2023-07-03',
+        playerOne: { name: 'Michael', surname: 'Brown' },
+        score: '6:2',
+        playerTwo: { name: 'Emma', surname: 'Davis' }
+    },
+    {
+        date: '2023-07-04',
+        playerOne: { name: 'David', surname: 'Miller' },
+        score: '6:1',
+        playerTwo: { name: 'Olivia', surname: 'Jones' }
+    },
+    {
+        date: '2023-07-05',
+        playerOne: { name: 'Daniel', surname: 'Wilson' },
+        score: '6:5',
+        playerTwo: { name: 'Sophia', surname: 'Martinez' }
+    },
+    {
+        date: '2023-07-06',
+        playerOne: { name: 'Matthew', surname: 'Taylor' },
+        score: '6:0',
+        playerTwo: { name: 'Isabella', surname: 'Anderson' }
+    }
+];
+</script>
+
+<template>
+    <main>
+        <div class="news">
+            <h2>Najnowsze informacje</h2>
+            <div class="news-wrapper">
+                <img src="../assets/images/news-croped.jpg" alt="" srcset="" />
+                <div class="text-container">
+                    <p>Zakończyliśmy zapisy do ligi Steel Dart Rzeszów w sezonie 2023/2034!</p>
+                </div>
+            </div>
+        </div>
+        <div class="league-tables">
+            <div class="modals">
+                <div class="leagues">
+                    <div>1 Liga</div>
+                    <div>2 Liga</div>
+                    <div>3 Liga</div>
+                </div>
+                <div class="round">Kolejka⬇️</div>
+            </div>
+            <div class="games">
+                <div v-for="game in games" class="game">
+                    <div class="date">{{ game.date }}</div>
+                    <div class="player-one">
+                        {{ game.playerOne.name + ' ' + game.playerOne.surname }}
+                    </div>
+                    <div class="score">{{ game.score }}</div>
+                    <div class="player-two">
+                        {{ game.playerTwo.name + ' ' + game.playerTwo.surname }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 1000px;
+    gap: 30px;
+}
+.league-tables,
+.news-wrapper {
+    background-color: #2e2e2e;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    border-radius: 20px;
+}
+.news-wrapper {
+    overflow: hidden;
+}
+.news > img {
+    object-fit: cover;
+    width: 100%;
+    height: 400px;
+    border-end-end-radius: 20px;
+    border-bottom-left-radius: 20px;
+}
+.text-container {
+    min-height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+p {
+    text-align: center;
+    font-size: 20px;
+}
+.league-tables {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+}
+.modals {
+    display: flex;
+    justify-content: space-between;
+}
+.leagues > div,
+.round {
+    background-color: #1e1e1e;
+    border-radius: 20px;
+    padding: 5px 30px;
+}
+.leagues {
+    display: flex;
+    gap: 10px;
+}
+.games {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+}
+.game {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    border: 3px solid #534843;
+    border-radius: 20px;
+    text-align: center;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+</style>

a418f179f660b5969ea1638a819f3cfcb4a72a02
Tucznik2 <f3kula@gmail.com>
2023-09-29 15:56:43 +0200

    feat: basic structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import NewGameView from '../views/NewGameView.vue';
+import PlayerView from '@/views/PlayerView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
             path: '/newGame',
             name: 'newGame',
             component: NewGameView
+        },
+        {
+            path: '/players/:id',
+            name: 'player',
+            component: PlayerView
         }
     ]
 });
---
+++ src/views/PlayerView.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts"></script>
+
+<template>
+    <main>
+        <h1>Profil zawodnika</h1>
+        <div class="container">
+            <div class="player-statistics-container">
+                <div class="player-stat"></div>
+                <h2>Filip Kula</h2>
+                <p>Liga: Pierwsza Liga</p>
+                <p>Średnia: 85.54</p>
+                <p>Punkty: 12</p>
+                <p>Bilans: 23</p>
+                <p>Legi wygrane: 34</p>
+                <p>Legi przegrane: 22</p>
+                <p>Ilość maksów: 12</p>
+
+                <div>
+                    <p>High Outs:</p>
+                    <ul>
+                        <!-- <li v-for="highOut in player.highOuts" :key="highOut">{{ highOut }}</li> -->
+                        <li>123</li>
+                        <li>125</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p>Fast Outs:</p>
+                    <ul>
+                        <!-- <li v-for="fastOut in player.fastOuts" :key="fastOut">{{ fastOut }}</li> -->
+                        <li>12</li>
+                        <li>14</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 1000px;
+    /* gap: 30px; */
+}
+</style>

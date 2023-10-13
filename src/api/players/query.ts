7b13b5b6e49eb040cc8c8ab48f360c8597335fb3
Filip Kula <f3kula@gmail.com>
2023-10-13 11:44:24 +0200

    feat: desktop league view

--- src/api/players/query.ts
+++ src/api/players/query.ts
@@ -1,6 +1,10 @@
 import { useQuery } from '@tanstack/vue-query';
-import { fetchPlayerInfo } from './player';
+import { fetchLeaguePlayers, fetchPlayerInfo } from './player';
 
 export const useGetPlayerInfo = (playerId: string) => {
     return useQuery(['player-info', playerId], () => fetchPlayerInfo(playerId));
+};
+
+export const useGetLeaguePlayers = (leagueId: string) => {
+    return useQuery(['league-players', leagueId], () => fetchLeaguePlayers(leagueId));
 };
--- src/views/LeagueView.vue
+++ src/views/LeagueView.vue
@@ -1,97 +1,12 @@
 <script setup lang="ts">
-const players = [
-    {
-        id: 1,
-        name: 'John',
-        secondName: 'Doe',
-        points: 501,
-        balance: -20,
-        legsWon: 10,
-        legsLost: 5,
-        average: 75.23,
-        maxAmount: 180,
-        highOuts: [100, 120, 140],
-        fastOuts: [9, 12, 15]
-    },
-    {
-        id: 2,
-        name: 'Alice',
-        secondName: 'Smith',
-        points: 501,
-        balance: 0,
-        legsWon: 7,
-        legsLost: 8,
-        average: 63.45,
-        maxAmount: 170,
-        highOuts: [100, 130, 150],
-        fastOuts: [10, 13, 18]
-    },
-    {
-        id: 3,
-        name: 'Bob',
-        secondName: 'Johnson',
-        points: 501,
-        balance: 15,
-        legsWon: 12,
-        legsLost: 4,
-        average: 85.67,
-        maxAmount: 177,
-        highOuts: [100, 110, 120],
-        fastOuts: [11, 14, 17]
-    },
-    {
-        id: 4,
-        name: 'Sarah',
-        secondName: 'Brown',
-        points: 501,
-        balance: -10,
-        legsWon: 8,
-        legsLost: 6,
-        average: 70.89,
-        maxAmount: 160,
-        highOuts: [100, 125, 145],
-        fastOuts: [9, 13, 16]
-    },
-    {
-        id: 5,
-        name: 'David',
-        secondName: 'Wilson',
-        points: 501,
-        balance: 25,
-        legsWon: 14,
-        legsLost: 3,
-        average: 92.34,
-        maxAmount: 173,
-        highOuts: [100, 135, 155],
-        fastOuts: [10, 14, 18]
-    },
-    {
-        id: 6,
-        name: 'Emily',
-        secondName: 'Jones',
-        points: 501,
-        balance: -5,
-        legsWon: 9,
-        legsLost: 7,
-        average: 68.76,
-        maxAmount: 165,
-        highOuts: [100, 128, 148],
-        fastOuts: [11, 15, 17]
-    },
-    {
-        id: 7,
-        name: 'Michael',
-        secondName: 'Clark',
-        points: 501,
-        balance: 10,
-        legsWon: 11,
-        legsLost: 5,
-        average: 80.45,
-        maxAmount: 175,
-        highOuts: [100, 118, 138],
-        fastOuts: [12, 16, 18]
-    }
-];
+import { useGetLeaguePlayers } from '@/api/players/query';
+import { useRoute } from 'vue-router';
+
+const {
+    params: { id }
+} = useRoute();
+
+const { data: players } = useGetLeaguePlayers(id as string);
 </script>
 
 <template>
@@ -112,27 +27,27 @@ const players = [
                 <th>Wysokie kończenia</th>
                 <th>Szybkie kończenia</th>
             </thead>
-            <tbody>
-                <tr v-for="player in players" :key="player.id">
+            <tbody v-if="players">
+                <tr v-for="player in players.data.data" :key="player.id">
                     <td>{{ player.name }}</td>
                     <td>{{ player.secondName }}</td>
                     <td>{{ player.points }}</td>
                     <td>{{ player.balance }}</td>
                     <td>{{ player.legsWon }}</td>
                     <td>{{ player.legsLost }}</td>
-                    <td>{{ player.average }}</td>
+                    <td>{{ player.average3Darts }}</td>
                     <td>{{ player.maxAmount }}</td>
                     <td>
                         <ul>
                             <li v-for="(highOut, index) in player.highOuts" :key="index">
-                                {{ highOut }}
+                                {{ highOut.value }}
                             </li>
                         </ul>
                     </td>
                     <td>
                         <ul>
                             <li v-for="(fastOut, index) in player.fastOuts" :key="index">
-                                {{ fastOut }}
+                                {{ fastOut.value }}
                             </li>
                         </ul>
                     </td>

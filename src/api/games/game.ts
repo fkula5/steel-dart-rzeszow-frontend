2aa18e225fe685b74c092208e63b94c53ae98b9c
Filip Kula <f3kula@gmail.com>
2023-10-20 21:57:45 +0200

    feat: recent games by league request

--- src/api/games/game.ts
+++ src/api/games/game.ts
@@ -3,3 +3,7 @@ import { $axiosInstance } from '@/plugins/axios';
 export const fetchGames = () => {
     return $axiosInstance.get<unknown>('/games');
 };
+
+export const fetchRecentGamesByLeague = (leagueId: string) => {
+    return $axiosInstance.get<unknown>(`/recentLeagueGames/${leagueId}`);
+};
